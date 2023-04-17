cfb0a77156f50ed5b20dbb125579503650aedbc3
Appeasure <shawndavonlowe@gmail.com>
2023-04-17 14:46:01 -0500

    test

--- songcode.ino
+++ songcode.ino
@@ -1,5 +1,18 @@
+// Notes
+#define NOTE_F1
+#define NOTE_C1
+#define NOTE_A1
+#define NOTE_G1
+
+
 void setup() {
+ // Shawn code
+ int tempo = 144
   // Anthony code section
+  int C4 = 262;
+  // Shawn code section
+  int D4 = 330;
+
 }
 
 void loop() {
