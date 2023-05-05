245c5d93d644dcdf822ca888d868d0e93cb5b5d4
maxster-code <max.lozano@my.utsa.edu>
2023-05-05 12:50:05 -0500

    UPADTED CODE PARTS

--- songcode.ino
+++ songcode.ino
@@ -1,5 +1,5 @@
 /* 
-  Safe and Sound - Capital City                                    
+  Safe and Sound - Capital City (TRUMPET SECTION)                              
                                               
   EPICS Group 4, 2023
   Max, Shawn, Anthony, & Carlos
@@ -66,7 +66,7 @@ int QUARTER_NOTE = 4;
 int EIGHTH_NOTE = 8;
 
 int trumpetMelody[] = {
-   // Safe and Sound   
+   // ANTHONY  
   C4, 8, C4, 8, C4, 8, REST, 8, 
   E4, 8, E4, 8, REST, 2, REST, 4,
   G4, 8, G4, 8, G4, 8, E4, 8, 
@@ -82,7 +82,7 @@ int trumpetMelody[] = {
   C4, 8, G4, 8, G4, 8, E4, 8, 
   E4, 8, d4, 8, d4, 8, E4, 8, 
   F4, 8, E4, 8,
-  //trumpet enters
+  //trumpet enters CARLOS
   E4, 4, A3, 4, B3, 8, C4, 8, 
   d4, 8, E4, 4, G3, 8, REST, 8, G3, 8, B3, 8, C4, 8, d4, 8, 
   REST, 8, E4, 4, A3, 4, B3, 8, C4, 8, 
@@ -109,7 +109,7 @@ void setup() {
   digitalWrite(12, HIGH);
   
 }
-
+//MAX
 void loop() {
   if (digitalRead(button1Pin) == 0)  //funcitons based off of button pulling input pin LOW
   {
@@ -145,3 +145,140 @@ void loop() {
     }
   }
 }
+/* 
+  Safe and Sound - Capital City (BASS SECTION)                                
+                                              
+  EPICS Group 4, 2023
+  Max, Shawn, Anthony, & Carlos
+*/
+
+const int button1Pin = 4;
+int run;
+
+#define C1 33
+#define D1 37
+#define E1 41
+#define F1 44
+#define G1 49
+#define A1 55
+#define B1 62
+#define C2 65
+#define D2 73
+#define E2 82
+#define F2 87
+#define G2 98
+#define A2 110
+#define B2 123
+#define C3 131
+#define D3 147
+#define E3 165
+#define F3 175
+#define G3 196
+#define A3 220
+#define B3 247
+#define C4 262
+#define d4 294
+#define E4 330
+#define F4 349
+#define G4 392
+#define A4 440
+#define B4 494
+#define C5 523
+#define D5 587
+#define E5 659
+#define F5 698
+#define G5 784
+#define A5 880
+#define B5 988
+#define C6 1047
+#define REST 0
+
+// this alters the speed of the array
+//this variable is connected to the void setup if-then statements
+
+int tempo = 105;
+
+// indicates buzzer pin
+
+int bassMelodybuzzer = 13;
+
+// notes’ pitch, duration of note
+// 1 means whole note, 4 a quarter note, 8 an eighteenth note, 16 sixteenth note
+// IMPORTANT NOTICE: negative numbers in the array refer to dotted notes
+// Example: -4 means a dotted quarter note, or -8 dotted eighteenth note
+
+int WHOLE_NOTE = 1;
+int HALF_NOTE = 2;
+int QUARTER_NOTE = 4;
+int EIGHTH_NOTE = 8;
+int TWELTH_NOTE = 12;
+
+int bassMelody[] = {
+  // CARLOS & MAX
+  F2, 8, A2, 8, F2, 8, A2, 8,
+  F2, 8, A2, 8, F2, 8, A2, 8,
+  C2, 8, E2, 8, C2, 8, E2, 8,
+  C2, 8, E2, 8, C2, 8, E2, 8,
+  A1, 8, C2, 8, A1, 8, C2, 8,
+  A1, 8, C2, 8, A1, 8, C2, 8,
+  G1, 8, B1, 8, G1, 8, B1, 8,
+  G1, 8, B1, 8, G1, 8, B1, 8,
+  
+  F2, 8, A2, 8, F2, 8, A2, 8,
+  F2, 8, A2, 8, F2, 8, A2, 8,
+  C2, 8, E2, 8, C2, 8, E2, 8,
+  C2, 8, E2, 8, C2, 8, E2, 8,
+  A1, 8, C2, 8, A1, 8, C2, 8,
+  A1, 8, C2, 8, A1, 8, C2, 8,
+  G1, 8, B1, 8, G1, 8, B1, 8,
+  G1, 8, B1, 8, 
+  
+  F2, 1, C2, 1, A2, 1, G2, 1, REST, 8,
+  
+};
+
+//MAX
+
+
+// sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
+// there are two values per note (pitch and duration), so for each note there are four bytes
+int notes = sizeof(bassMelody) / sizeof(bassMelody[0]) / 2;
+
+// this calculates the duration of a whole note in ms (60s/tempo)*4 beats (milisonds to bpm)
+int wholenote = (60000 * 4) / tempo;
+
+int divider = 0, noteDuration = 0;
+
+void setup() {
+  pinMode(7, INPUT);
+}
+
+void loop() {
+while (digitalRead(7) == LOW)  {
+  delay(10); 
+}
+
+    // iterate over the notes of the melody.
+    // the array is twice the number of notes (notes + durations)
+    for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+
+      divider = bassMelody[thisNote + 1];
+      if (divider > 0) {
+        // regular note, just proceed
+        noteDuration = (wholenote) / divider;
+      } else if (divider < 0) {
+        // dotted notes are represented with negative durations!!
+        noteDuration = (wholenote) / abs(divider);
+        noteDuration *= 1.5;  // increases the duration in half for dotted notes
+      }
+
+      // Plays notes for 90% of the duration, and gives 10% leeway (this resolves the resonance of the piazza buzzer
+      tone(bassMelodybuzzer, bassMelody[thisNote], noteDuration * 0.9);
+      // Wait for the specify the duration of the note before playing the next note.
+      delay(noteDuration);
+
+      // stop the waveform generation before the next note.
+      noTone(bassMelodybuzzer);
+    }
+  }
+
